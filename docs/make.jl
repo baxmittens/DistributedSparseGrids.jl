ad8afca0b88d2cf6be0b6ac82b2a2fe8d318ce4f
Bittens <Bittens.M@bgr.local>
2022-10-13 14:48:16 +0200

    ins

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,13 @@
 import Pkg; Pkg.add("Documenter")
 Pkg.add("StaticArrays")
+Pkg.add("Colors")
+Pkg.add("PlotlyJS")
+Pkg.add("UnicodePlots")
+Pkg.add("FastGaussQuadrature")
+
 push!(LOAD_PATH,"../src/")
-include("../src/DistributedSparseGrids.jl")
+include("../src/DistributedSparseGrids.jl")#
+
 using Documenter, Main.DistributedSparseGrids
 makedocs(
 	sitename = "DistributedSparseGrids.jl",
@@ -14,3 +20,9 @@ makedocs(
 deploydocs(
     repo = "github.com/baxmittens/DistributedSparseGrids.jl.git"
 )
+
+
+Colors = "5ae59095-9a9b-59fe-a467-6f913c188581"
+PlotlyJS = "f0f68f2c-4968-5e81-91da-67840de0976a"
+UnicodePlots = "b8865327-cd53-5732-bb35-84acbb429228"
+FastGaussQuadrature = "442a2c76-b920-505d-bb47-c5924d526838"
