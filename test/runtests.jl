6d088e89f18bc99d21d4ccfc32b5d7b68961fbf0
Bittens <Bittens.M@bgr.local>
2022-10-13 13:50:53 +0200

    ins

--- test/runtests.jl
+++ test/runtests.jl
@@ -40,11 +40,13 @@ end
 init_weights!(asg, fun1)
 
 # adaptive refine
-for i = 1:5
+for i = 1:12
   # call generate_next_level! with tol=1e-5 and maxlevels=20
-  cpts = generate_next_level!(asg, 1e-5, 3)
+  cpts = generate_next_level!(asg, 1e-4, 12)
   # calculate weights on all worker
   distributed_init_weights!(asg, collect(cpts), fun1, ar_worker)
 end
+
+@test  isapprox(2.33905, integrate(asg), atol=1e-3)
 
 end
